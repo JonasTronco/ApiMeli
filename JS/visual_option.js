7f3a6d37d97429c645ef1d196a44576204f096bb
JonasTronco <infanpearl@gmail.com>
2019-01-27 01:28:54 -0500

    Solucion bug del Buscardor

--- JS/visual_option.js
+++ JS/visual_option.js
@@ -1,14 +1,9 @@
-$(document).ready(function() {
-    document.addEventListener('DOMContentLoaded', function() {
-        var checkPageButton = document.getElementById('melilookup');
-        checkPageButton.addEventListener('click', function() {
-          chrome.tabs.getSelected(null, function(tab) {
-            d = document
-            var f = d.createElement ('a');
-            f.onclick = window.open('api_mp.html','_blank','width=300,height=500','titlebar=no','status=no','location=no','left=100');
-            f.href = "#"
-            d.body.appendChild(f);
-          });
-        }, false);
-       }, false);
-    })
+
+$(function() 
+{
+  console.log( "Ha ocurrido document.ready: documento listo" )
+  var checkPageButton = document.getElementById('melilookup')
+  checkPageButton.addEventListener('click', function() {
+  window.open('api_mp.html','_blank','width=300,height=500','titlebar=no','status=no','location=no','left=100')
+  })
+});
